ada14f53d5fc2a8fcc9beaa9a80c0e4df3fe6b58
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-03-26 20:01:25 -0300

    Aula 19 - Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -4,25 +4,24 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/newproject">New Project</Link>
-      </div>
-      <Container customClass="min-height">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/newproject" element={<NewProject />} />
-        </Routes>
-      </Container>
-      <p>Footer</p>
+      <Navbar/>
+        <Container customClass="min_height">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/newproject" element={<NewProject />} />
+          </Routes>
+        </Container>
+      <Footer/>
     </Router>
   );
 }
