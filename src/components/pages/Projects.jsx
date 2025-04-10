f732ac683bbda3424e0563caaa9c98e8c93b565c
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-04-10 10:51:52 -0300

    Aula 26 - Criando Dashboard

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,5 +1,9 @@
 import Message from "../layout/Message"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 import { useLocation } from "react-router-dom"
+import styles from "./Projects.module.css"
+
 
 function Projects(){
     const location = useLocation()
@@ -9,9 +13,15 @@ function Projects(){
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar projeto"/>
+            </div>
             {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos</p>
+            </Container>
         </div>
     )
 }
