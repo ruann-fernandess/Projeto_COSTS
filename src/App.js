73f9a952ffc3a8689359406a22942d023dc0576f
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-05-19 22:33:48 -0300

    Aula 30 - Página de edição de dados

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
 
 //json-server --watch db.json --port 5000
 function App() {
@@ -15,11 +16,13 @@ function App() {
       <Navbar/>
         <Container customClass="min_height">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/company" element={<Company />} />
             <Route path="/newproject" element={<NewProject />} />
+            <Route path="/project/:id" element={<Project />} />
+
           </Routes>
         </Container>
       <Footer/>
