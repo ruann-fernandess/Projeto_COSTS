d5d35e6558925799ff55ff4950836b45d8ec0a6c
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-06-09 20:45:43 -0300

    Correção de uma palavra

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home(){
     return(
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
-            <p>Comece a gerenciaar os seus projetos agora mesmo!</p>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkButton to="/newproject" text="Criar projeto"/>
             <img src={savings} alt="Costs" />
         </section>
