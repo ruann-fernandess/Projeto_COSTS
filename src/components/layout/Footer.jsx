0aed50b7801957678dfa519385f766f455543bd1
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-06-09 18:57:07 -0300

    Atualizando redes sociais

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,17 +1,35 @@
 import styles from './Footer.module.css'
-import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
-function Footer (){
-    return(
-        <footer className={styles.footer}>
-            <ul className={styles.social_list}>
-                <li><FaFacebook/></li>
-                <li><FaInstagram/></li>
-                <li><FaLinkedin/></li>
-            </ul>
-            <p className={styles.copy_right}><span>Costs</span> &copy; 2025</p>
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <ul className={styles.social_list}>
+
+        <li>
+            <a href="https://github.com/ruann-fernandess" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                <FaGithub />
+            </a>
+        </li>
+
+        <li>
+          <a href="https://www.instagram.com/ruann_fernandess" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <FaInstagram />
+          </a>
+        </li>
+
+        <li>
+          <a href="https://www.linkedin.com/in/ruann-fernandess" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+            <FaLinkedin />
+          </a>
+        </li>
+
+      </ul>
+      <p className={styles.copy_right}>
+        <span>Costs</span> &copy; 2025
+      </p>
+    </footer>
+  )
 }
 
 export default Footer
