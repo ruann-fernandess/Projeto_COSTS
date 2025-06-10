6751f06dd53f2a4283fa76d2bf3b3a5eff82e0de
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-06-10 00:15:46 -0300

    Atualizando Company.jsx

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -1,6 +1,13 @@
-function Company(){
-    return(
-        <h1>Company</h1>
+import styles from './Company.module.css'
+import LinkButton from '../layout/LinkButton'
+
+function Company() {
+    return (
+        <section className={styles.company_container}>
+            <h1>Conheça a <span>Costs</span></h1>
+            <p>Somos uma empresa dedicada a ajudar você a gerenciar seus projetos de forma eficiente e simples.</p>
+            <LinkButton to="/about" text="Saiba mais"/>
+        </section>
     )
 }
 
