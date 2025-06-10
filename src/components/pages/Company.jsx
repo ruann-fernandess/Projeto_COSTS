b659f57f23ced5e3ad5d4ebd6165d8da31da2c2e
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-06-10 00:22:05 -0300

    Atualizando Contact.jsx

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -6,7 +6,7 @@ function Company() {
         <section className={styles.company_container}>
             <h1>Conheça a <span>Costs</span></h1>
             <p>Somos uma empresa dedicada a ajudar você a gerenciar seus projetos de forma eficiente e simples.</p>
-            <LinkButton to="/about" text="Saiba mais"/>
+            <LinkButton to="/Company" text="Saiba mais"/>
         </section>
     )
 }
